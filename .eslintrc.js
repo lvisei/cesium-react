fd20057d17b82c7b93a8b997486ad1b29af4c22e
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2019-06-14 17:10:27 +0800

    chore: config conflict with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: ['react-app', 'prettier'],
+  extends: ['react-app', 'prettier', 'prettier/react'], 
+  // eslint-config-prettier
+  // Turns off all rules that are unnecessary or might conflict with Prettier
+  // https://github.com/prettier/eslint-config-prettier
   plugins: ['prettier'],
   rules: {
     'no-console': 'off',
